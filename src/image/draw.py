c3404f3f5a6767e721d5d6c67a8502e51df2d662
coming1024 <chengyihengok@126.com>
2022-06-15 22:28:21 +0800

    4

--- src/image/draw.py
+++ src/image/draw.py
@@ -13,24 +13,29 @@ def getMidPoint(x1, y1, x2, y2):
 def getMidPoint(point1, point2):
     return getMidPoint(point1.x, point1.y, point2.x, point2.y)
 
-def drawRectangle(img,x1,y1,x2,y2):
-    cv2.rectangle(img,(x1,y1),(x2,y2),PEN.penColor,PEN.penThickness)
+
+def drawRectangle(img, x1, y1, x2, y2):
+    cv2.rectangle(img, (x1, y1), (x2, y2), PEN.penColor, PEN.penThickness)
 
 
-def drawCircle(img,x1,y1,x2,y2):
-    x,y=(x1+x2)/2,(y1+y2)/2
+def drawCircle(img, x1, y1, x2, y2):
+    x, y = (x1 + x2) / 2, (y1 + y2) / 2
+    x = int(x)
+    y = int(y)
 
-    r=math.sqrt(pow(x2-x1,2)+pow(y1-y2,2))/2.0
-    cv2.circle(img, (x, y), r, PEN.penColor, PEN.penThickness)
+    r = math.sqrt(pow(x2 - x1, 2) + pow(y1 - y2, 2)) / 2.0
+    r = int(r)
+    print(x, y, r)
+    cv2.circle(img, (x, y), r, PEN.penColor,PEN.penThickness)
+
 
 def drawTriangle(img, x1, y1, x2, y2):
-    point1=(x2,y2)
-    point2=(x1+40,y1)
-    point3=(x1-40,y1)
+    point1 = (x2, y2)
+    point2 = (x1 + 40, y1)
+    point3 = (x1 - 40, y1)
     cv2.line(img, point1, point2, PEN.penColor, PEN.penThickness)
     cv2.line(img, point2, point3, PEN.penColor, PEN.penThickness)
     cv2.line(img, point1, point3, PEN.penColor, PEN.penThickness)
-
 
 # def drawRectangle(img, row, col, length, width, angle):
 #     angle *= (10 / 180) * math.pi  # 弧度
